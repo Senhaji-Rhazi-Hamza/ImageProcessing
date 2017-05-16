099a6bd31ef44d9f08b2697870b0efba58f2a6a3
nagielhachem <nagielhachem@gmail.com>
2017-05-16 15:11:16 +0200

    src/image.py update: change 'dog' to 'DoG'

--- src/image.py
+++ src/image.py
@@ -9,14 +9,14 @@ class Image:
         self.kparam = 2
         self.scales = None
         #self.octaves = None
-        self.dogs = None
+        self.DoGs = None
         self.size = min(height, width)
-        self.img = cv2.resize(self.img,(self.size, self.size),
+        self.img = cv2.resize(self.img, (self.size, self.size),
                 interpolation = cv2.INTER_CUBIC)
 
  
     def show(self):
-        cv2.imshow('image',self.img)
+        cv2.imshow('image', self.img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
  
@@ -31,7 +31,7 @@ class Image:
     def show_scale(self, scale):
         if self.scales is None:
             self.set_scales()
-        cv2.imshow('image',self.scales[scale])
+        cv2.imshow('image', self.scales[scale])
         cv2.waitKey(0)
         cv2.destroyAllWindows()
  
@@ -42,22 +42,22 @@ class Image:
         self.show_scale(len(self.scales) - 1)
  
  
-    def set_dogs(self):
+    def set_DoGs(self):
         ngrad = 5
         if self.scales is None:
             self.set_scales()
-        if self.dogs is None:
+        if self.DoGs is None:
             octaves = [[cv2.GaussianBlur(self.scales[j], (5, 5),
                 sigmaX = 1.6 * self.kparam ** i) 
                 for i in range(ngrad)]
                 for j in range(len(self.scales))]
-        self.dogs = [[octaves[j][i] - octaves[j][i + 1] 
+        self.DoGs = [[octaves[j][i] - octaves[j][i + 1] 
             for i in range(ngrad - 1)]
             for j in range(len(self.scales))]
  
  
-    def show_dog(self, i, j):
-        if self.dogs is None:
-            self.set_dogs()
-        cv2.imshow('image',self.dogs[i][j])
+    def show_DoG(self, i, j):
+        if self.DoGs is None:
+            self.set_DoGs()
+        cv2.imshow('image',self.DoGs[i][j])
         cv2.waitKey(0)
