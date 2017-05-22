a4758b6655b2a6f998fb6d94453ee989a72eefa8
nagielhachem <nagielhachem@gmail.com>
2017-05-22 20:38:34 +0200

    main update: send initial sigma as argument

--- src/main.py
+++ src/main.py
@@ -5,14 +5,13 @@ from sift import SIFT
 
 
 def main(argv):
-    if len(argv) == 1:
-        print "Usage Error: python" ,argv[0] + "image_name"
+    if len(argv) < 3:
+        print "Usage Error: python" ,argv[0] + "image_name sigma" 
         return
     img = cv2.imread(argv[1])
-    sift = SIFT()
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    sift = SIFT(float(argv[2]))
     sift.extractFeatures(img)
-    sift.showOcatves()    
-    sift.showDoG()
 
 if __name__ == "__main__" :
     main(sys.argv[:])
