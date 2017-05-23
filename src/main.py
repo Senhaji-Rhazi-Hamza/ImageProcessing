70147cba2678ff51c1f131be8c7c7364e4f33804
nagielhachem <nagielhachem@gmail.com>
2017-05-23 16:25:54 +0200

    reformating code

--- src/main.py
+++ src/main.py
@@ -1,17 +1,25 @@
 import sys
 import cv2
 import numpy as np
+import math
 from sift import SIFT
 
 
 def main(argv):
-    if len(argv) < 3:
-        print "Usage Error: python" ,argv[0] + "image_name sigma" 
+    if len(argv) < 2:
+        print "Usage Error: python" ,argv[0] + "image_name [sigma]" 
         return
+    
+    sigma = 1.6
+    k = math.sqrt(2)
+    if len(argv) > 2:
+        sigma = float(argv[2])
+    if len(argv) > 3:
+        k = float(argv[3])
     img = cv2.imread(argv[1])
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    sift = SIFT(float(argv[2]))
-    sift.extractFeatures(img)
+    sift = SIFT(sigma, k)
+    sift.extract_features(img)
 
 if __name__ == "__main__" :
     main(sys.argv[:])
--- src/sift.py
+++ src/sift.py
@@ -6,44 +6,47 @@ import math
 
 class SIFT:
     
-    def __init__(self, sigma):
-        self.k = math.sqrt(2)
+    def __init__(self, sigma, k):
+        self.k = k
         self.sigma = sigma
-        s = math.sqrt(2)
-        self.sigma0 = [s/2, s, 2 * s, 4 * s]
+        # s = math.sqrt(2)
+        # self.sigma0 = [s/2, s, 2 * s, 4 * s]
         self.scaleLvl = 5
         self.octaveLvl = 4
         self.DoGLvl = self.scaleLvl - 1
 
-    def extractFeatures(self, image):
-        height, width = image.shape[:2]
-        size = min(height, width)
-        octaves = self.generateOctaves(image)
-#        self.showOcatves(octaves)
-        DoG = self.generateDoG(octaves)
-        self.showDoG(DoG)
-        extremum = self.getExtremum(DoG)
-        self.showExtremum(extremum)
+    def extract_features(self, image):
+        pyramide = self.build_pyramid(image)
+        octaves = self.build_octaves(pyramide)
+        self.show_images(octaves, 1, self.scaleLvl)
+        DoG = self.build_DoG(octaves)
+        self.show_images(DoG, self.octaveLvl, self.DoGLvl)
+#        extremum = self.compute_extrema(DoG)
+#        self.show_images(extremum, self.octaveLvl, 1)
     
-    def generateOctaves(self, image):
-        # generate pyramide (octaveLvl different sizes):
+
+    # generate pyramide (octaveLvl different sizes):
+    def build_pyramid(self, image):
         pyramide = [cv2.resize(image, None, fx = 2 ** -(i), 
             fy = 2 ** -(i), interpolation = cv2.INTER_CUBIC) 
             for i in range(self.octaveLvl)]
-        # apply gaussian filter on pyramide to generate different octave/scales 
+        return pyramide
+
+    # apply gaussian filter on pyramide to generate different octave/scales 
+    def build_octaves(self, pyramide):
         octaves = [[cv2.GaussianBlur(pyramide[i], ksize = (0, 0),
-            sigmaX = self.sigma * self.k ** j, sigmaY = 0) 
+            sigmaX = self.sigma * self.k ** j) 
             for j in range(self.scaleLvl)] 
             for i in range(self.octaveLvl)]
         return octaves
 
-    def generateDoG(self, octaves):
+    def build_DoG(self, octaves):
         DoG = [[octaves[i][j + 1] - octaves[i][j] 
             for j in range(self.DoGLvl)]
             for i in range(self.octaveLvl)]
         return DoG
 
-    def getExtremum(self, DoG):
+    def compute_extrema(self, DoG):
         extremum = [DoG[i][0].copy() for i in range(self.octaveLvl)]
         for i in range (self.octaveLvl):
             for j in range (1, self.DoGLvl - 1):
@@ -59,26 +62,20 @@ class SIFT:
                             extremum[i][k, l] = 255
         return extremum
 
-    def showOcatves(self, octaves):
-        for i in range (self.octaveLvl):
-            for j in range (self.scaleLvl):
+    def show_images(self, images, n, m):
+        print("Showing a group of images. \
+                \nPress any key to show next image.\
+                \nPress 'q' to exit.")
+        for i in range (n):
+            for j in range (m):
+                if m == 1:
+                    im = images[i]
+                else:
+                    im = images[i][j]
                 img = 'image [' + str(i) + '][' + str(j) + ']'
-                cv2.imshow(img, octaves[i][j])
-                cv2.waitKey(0)
+                cv2.imshow(img, im)
+                if cv2.waitKey(0) == 113:
+                    cv2.destroyAllWindows()
+                    return
                 cv2.destroyAllWindows()
  
-    def showDoG(self, DoG):
-        for i in range (self.octaveLvl):
-            for j in range (self.DoGLvl):
-                img = 'DoG [' + str(i) + '][' + str(j) + ']'
-                cv2.imshow(img, DoG[i][j])
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()
-
-    def showExtremum(self, extremum):
-        for i in range (self.octaveLvl):
-            img = 'Extremum [' + str(i) + ']'
-            cv2.imshow(img, extremum[i])
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
-
