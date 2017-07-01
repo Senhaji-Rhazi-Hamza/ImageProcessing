fa177a2a2b93a9e49b79a7f906059548ef20fd5b
nagielhachem <nagielhachem@gmail.com>
2017-07-01 14:14:22 +0200

    main updated, parse arguments

--- src/main.py
+++ src/main.py
@@ -5,20 +5,32 @@ import math
 from sift import SIFT
 
 
+def parse_arguments(argv):
+    n = len(argv)
+    sigma, k = 1.6, math.sqrt(2)
+    if n < 2:
+        print("Usage: python {} image_path \
+                [-s value] [-k value]".format(argv[0]))
+        exit(1)
+    if n > 6:
+        if argv[2] == '-s' and argv[4] == '-k':
+            sigma = float(argv[3])
+            k = float(argv[5])
+        if argv[2] == '-k' and argv[4] == 's':
+            k = float(argv[3])
+            sigma = float(argv[5])
+    if n > 4:
+        if argv[2] == '-s':
+            sigma = float(argv[3])
+        if argv[2] == '-k':
+            k = float(argv[3])
+   return sigma, k 
+
 def main(argv):
-    if len(argv) < 2:
-        print "Usage Error: python" ,argv[0] + "image_name [sigma]" 
-        return
-    
-    sigma = 1.6
-    k = math.sqrt(2)
-    if len(argv) > 2:
-        sigma = float(argv[2])
-    if len(argv) > 3:
-        k = float(argv[3])
+    sigma, k = parse_arguments(argv)
+    sift = SIFT(sigma, k)
     img = cv2.imread(argv[1])
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    sift = SIFT(sigma, k)
     sift.extract_features(img)
 
 if __name__ == "__main__" :
