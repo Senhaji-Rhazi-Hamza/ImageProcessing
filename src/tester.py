12857dc7a20dd74c766fc3e1923ebc847f870069
Senhaji-Rhazi-Hamza <hamza.senhaji-rhazi@epita.fr>
2017-07-10 14:35:17 +0200

    tester modified

--- src/tester.py
+++ src/tester.py
@@ -1,10 +1,8 @@
 import numpy as np
 
 def distances(descs1, descs2):
-    m = len(descs1)
-    n = len(descs2)
-    a = np.array([[np.linalg.norm(descs1[i] - descs2[j]) for j in range(m)]\
-            for i in range(n)])
+    a = np.array([[np.linalg.norm(descs1[i] - descs2[j]) for j in range(descs2.shape[0])]\
+            for i in range(descs1.shape[0])])
     return a
 
 def getPairsMins(distances):
@@ -13,16 +11,23 @@ def getPairsMins(distances):
     pairs = np.array([(i,a[i][0]) for i in range(n)])
     minns = np.array([distances[i].min() for i in range(n)])
     idx = minns.argsort()
-    mins = mins[idx]
+    minns = minns[idx]
     pairs = pairs[idx]
-    return pairs, mins
+    return pairs, minns
 
-def match(kp1, des1, kp2, des2, k = 10):
+def match(kp1, des1, kp2, des2, k = 100):
+    des1 = np.concatenate((des1[0], des1[1], des1[2], des1[3]), axis = 0)
+    des2 = np.concatenate((des2[0], des2[1], des2[2], des2[3]), axis = 0)
+    kp1 = np.concatenate((kp1[0], kp1[1], kp1[2], kp1[3]), axis = 0)
+    kp2 = np.concatenate((kp2[0], kp2[1], kp2[2], kp2[3]), axis = 0)
     n, m = len(des1), len(des2)
     assert(k > 0), "number of key point must be > 0"
     size = min(n,m,k)
-    pairs, mins = getPairsMins(ditances(descs1, descs2))
+    pairs, mins = getPairsMins(distances(des1, des2))
     avg = mins[:k].mean()
-    return pairs[:k], avg
-
-
+    match = True if (avg < 1) else False
+    if (match):
+      print("the pictures match", "with avg ", avg)
+    else:
+      print("the pictures does not match","with avg ", avg)
+    return pairs[:k], avg, match
